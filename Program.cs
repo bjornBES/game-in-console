2af7024b9cf356c219aa45b11ce80a1bb16cab6a
BjornBES <bjorn@r66.dk>
2022-06-16 14:53:49 +0200

    adding a crafting system

--- Program.cs
+++ Program.cs
@@ -4,12 +4,19 @@ using game_in_console.enums;
 
 namespace game_in_console
 {
+    public class ShopItemsList
+    {
+        public string name;
+        public int Con;
+        public int cost;
+    }
     class Program
     {
         float PlayerX, PlayerY;
         Player Player;
         item item;
         Map Map;
+        crafting.CraftItems craft;
         string PlayerName;
         
         static void Main()
@@ -21,6 +28,9 @@ namespace game_in_console
         {
             Player = new Player();
             Player.Start();
+            craft = new crafting.CraftItems();
+            craft.Start();
+            craft.Player = Player;
             item = new item();
             Map = new Map();
             Map.Start();
@@ -28,39 +38,43 @@ namespace game_in_console
             PlayerY = Player.GetY();
             Console.WriteLine("Hello you there what is your name?");
             PlayerName = Console.ReadLine();
-            Console.WriteLine("Hello " + PlayerName);
-            Console.WriteLine("You: " + "where am i? and who are you?");
-            Console.WriteLine("i am Bolvar and you are in Strombard");
-            Console.WriteLine("You: " + "you said where?");
-            Console.WriteLine("Bolvar: " + "do you have a map?");
-            Console.WriteLine("say no to lie or say yes to not lie");
-            string User = Console.ReadLine();
-            if(User == "yes" || User == "Yes")
+            if(PlayerName != "skipto")
             {
-                Console.WriteLine("You: " + "yes i have a map");
-                Console.WriteLine("Bolvar: " + "yes yes can you open it");
-            }
-            else
-            {
-                Console.WriteLine("You: " + "no i do not have a map");
-                Console.WriteLine("Bolvar: " + "ok here take this map and open it");
-            }
-            Console.WriteLine(@"say ""Map"" to open your map");
-            User = "";
-            User = Console.ReadLine();
-            if(User == "Map")
-            {
-                for (int i = 0; i < Map.WorldMap.Length; i++)
+                Console.WriteLine("Hello " + PlayerName);
+                Console.WriteLine("You: " + "where am i? and who are you?");
+                Console.WriteLine("i am Bolvar and you are in Strombard");
+                Console.WriteLine("You: " + "you said where?");
+                Console.WriteLine("Bolvar: " + "do you have a map?");
+                Console.WriteLine("say no to lie or say yes to not lie");
+                string User = Console.ReadLine();
+                if (User == "yes" || User == "Yes" || User == "y" || User == "Y")
                 {
-                    Console.WriteLine(Map.WorldMap[i]);
+                    Console.WriteLine("You: " + "yes i have a map");
+                    Console.WriteLine("Bolvar: " + "yes yes can you open it");
                 }
+                if (User == "no" || User == "No" || User == "n" || User == "N")
+
+                {
+                    Console.WriteLine("You: " + "no i do not have a map");
+                    Console.WriteLine("Bolvar: " + "ok here take this map and open it");
+                }
+                Console.WriteLine(@"say ""Map"" to open your map");
+                User = "";
+                User = Console.ReadLine();
+                if (User == "WorldMap" || User == "worldmap" || User == "wm" || User == "WM")
+                {
+                    for (int i = 0; i < Map.WorldMap.Length; i++)
+                    {
+                        Console.WriteLine(Map.WorldMap[i]);
+                    }
+                }
+                Console.WriteLine("You: " + "what is Firestormb and what is the wild?");
+                Console.WriteLine("Bolvar: " + "Firestromb is a friend and the wild is a place where no one has gone to in over 1000 years");
+                Console.WriteLine("You: " + "what... over 1000 years :O ");
+                Console.WriteLine("Bolvar: " + "if you want to go there you need to train for it");
+                Console.WriteLine("Bolvar: " + "get your gear at the shop");
+                Console.WriteLine(@"say ""shop"" to go to the shop");
             }
-            Console.WriteLine("You: " + "what is Firestormb and what is the wild?");
-            Console.WriteLine("Bolvar: " + "Firestromb is a friend and the wild is a place where no one has gone to in over 1000 years");
-            Console.WriteLine("You: " + "what... over 1000 years :O ");
-            Console.WriteLine("Bolvar: " + "if you want to go there you need to train for it");
-            Console.WriteLine("Bolvar: " + "get your gear at the shop");
-            Console.WriteLine(@"say ""shop"" to go to the shop");
             Update();
         }
         bool First = true;
@@ -86,7 +100,7 @@ namespace game_in_console
                 Console.WriteLine(Player.coins);
             if (User == "Shop" || User == "shop" && Player.IsDun != true)
                 Shop(true, false);
-            if(User == "Inv")
+            if(User == "Inv" || User == "inv")
             {
                 Console.WriteLine("you have");
                 for (int i = 0; i < Player.InvIndex; i++)
@@ -95,9 +109,7 @@ namespace game_in_console
                 }
             }
             if (User == "Craft" || User == "craft" && Player.IsDun != true)
-            {
-
-            }
+                Craft(true);
             if(User == "WorldMap" && Player.IsDun != true)
             {
                 for (int i = 0; i < Map.WorldMap.Length; i++)
@@ -130,38 +142,28 @@ namespace game_in_console
         int item1 = 0;
         int item2 = 0;
         int item3 = 0;
+        string[] Taple = { "em", "Sticks", "Stones" };
+        //"irons", "flints"
+        int[] Con = { 0, 2, 3 };
+        //2, 2
+        int[] cost = { 0, 10, 15, 20, 10 };
+        ShopItemsList[] itemsList = new ShopItemsList[3];
+        int ItemsListIndex;
         public void Shop(bool shop, bool start)
         {
-            string[] Taple = { "em" ,"Sticks", "Stones", "irons", "flints"};
-            int[] Con = { 0, 2 , 3, 2, 2 };
-            int[] cost = { 0, 10, 15, 20, 10};
+            int Items = 3;
+            for (int i = 0; i < itemsList.Length; i++)
+            {
+                itemsList[i] = new ShopItemsList();
+            }
             string Help = @"you can say ""GO"" or ""GoOut"" to Go out of the shop or if you want to buy stuff say ""what can i buy"" or ""WCIB"" to see what the shopkepper has in stok and then say ""buy"" to buy";
-            string[] ShopKeeperIn = { "hello what can i do for you?", "it's it not a good day", "The Shop has all the items you need :D" };
             string[] ShopKeeperOut = { "thanks you and bye", "bye", "farewell hero" };
             if (shop == true)
             {
                 Random RNG = new Random();
-                if (En == false)
-                {
-                    item1 = RNG.Next(0, Taple.Length);
-                    item2 = RNG.Next(0, Taple.Length);
-                    item3 = RNG.Next(0, Taple.Length);
-                    if (item2 == item1)
-                        item2 = RNG.Next(0, Taple.Length);
-                    if (item3 == item2)
-                        item3++;
-                    if (item3 == Taple.Length)
-                        item3 -= 2;
-                    if (item1 == 0)
-                        item1++;
-                    if (item2 == 0)
-                        item2+= 2;
-                    if (item3 == 0)
-                        item3+= 3;
-                    if(start == false)
-                        Console.WriteLine("the ShopKeeper: " + ShopKeeperIn[RNG.Next(0, ShopKeeperIn.Length)]);
-                    En = true;
-                }
+                if(En == false)
+                    S_setup(RNG, start);
+
                 string User = "";
                 if (start == true)
                     User = "WCIB";
@@ -174,78 +176,148 @@ namespace game_in_console
                     En = false;
                 }
                 if (User == "what can i buy" || User == "what can i buy?" || User == "WCIB")
-                {
-                    Console.WriteLine("the ShopKeeper: " + "i have " + Con[item1] + " " + Taple[item1] + " for " + cost[item1] + ", " +
-    Con[item2] + " " + Taple[item2] + " for " + cost[item2] + ", " +
-    Con[item3] + " " + Taple[item3] + " for " + cost[item3]);
-                    User = "";
-                    if (start == true)
-                        Shop(shop, false);
-                }
+                    S_WCIB();
                 if (User == "Help" || User == "help" || User == "_h")
                     Console.WriteLine(Help);
                 if (User == "Buy" || User == "buy")
-                {
-                    Console.WriteLine("the ShopKeeper: " + "what to buy?");
-                    Console.WriteLine("to a buy a thing you need to say the item number eg " + "item1 to buy " + Con[item1] + " " + Taple[item1] + " for " + cost[item1]);
-                    string UserBuy = Console.ReadLine();
-                    if (UserBuy == "item1" || UserBuy == "Item1")
-                    {
-                        if (Player.coins >= cost[item1])
-                        {
-                            Player.inv[Player.InvIndex] = Taple[item1];
-                            Player.invCon[Player.InvIndex] = Con[item1];
-                            Player.InvIndex++;
-                            Console.WriteLine("the ShopKeeper: " + "thank you for purchaseing " + Con[item1] + " " + Taple[item1] + " for " + cost[item1]);
-                            Player.coins -= cost[item1];
-                            Taple[item1] = "";
-                            Con[item1] = 0;
-                            cost[item1] = 0;
-                        }
-                        else
-                            Console.WriteLine("the ShopKeeper: " + "Sorry but you do not have enough coins you need " + cost[item1] + " coins");
-                    }
-                    if (UserBuy == "item2" || UserBuy == "Item2")
-                    {
-                        if (Player.coins >= cost[item2])
-                        {
-                            Player.inv[Player.InvIndex] = Taple[item2];
-                            Player.invCon[Player.InvIndex] = Con[item2];
-                            Player.InvIndex++;
-                            Console.WriteLine("the ShopKeeper: " + "thank you for purchaseing " + Con[item2] + " " + Taple[item2] + " for " + cost[item2]);
-                            Player.coins -= cost[item2];
-                            Taple[item2] = "";
-                            Con[item2] = 0;
-                            cost[item2] = 0;
-                        }
-                        else
-                            Console.WriteLine("the ShopKeeper: " + "Sorry but you do not have enough coins you need " + cost[item2] + " coins");
-                    }
-                    if (UserBuy == "item3" || UserBuy == "Item3")
-                    {
-                        if (Player.coins >= cost[item3])
-                        {
-                            Player.inv[Player.InvIndex] = Taple[item3];
-                            Player.invCon[Player.InvIndex] = Con[item3];
-                            Player.InvIndex++;
-                            Console.WriteLine("the ShopKeeper: " + "thank you for purchaseing " + Con[item3] + " " + Taple[item3] + " for " + cost[item3]);
-                            Player.coins -= cost[item3];
-                            Taple[item3] = "";
-                            Con[item3] = 0;
-                            cost[item3] = 0;
-                        }
-                        else
-                            Console.WriteLine("the ShopKeeper: " + "Sorry but you do not have enough coins you need " + cost[item3] + " coins");
-                    }
-                }
+                    S_Buy();
                 if (User == "what can i do in here" || User == "wcidih")
                 {
                     Console.WriteLine(@"say ""help"" to get help");
                 }
                 User = "";
                 if(EnR == false)
-                    Shop(shop, start);
+                    Shop(shop, false);
             }
+        }
+        void S_setup(Random RNG, bool start)
+        {
+            string[] ShopKeeperIn = { "hello what can i do for you?", "it's it not a good day", "The Shop has all the items you need :D" };
+            item1 = RNG.Next(0, Taple.Length);
+            item2 = RNG.Next(0, Taple.Length);
+            item3 = RNG.Next(0, Taple.Length);
+            if (item2 == item1)
+                item2 = RNG.Next(0, Taple.Length);
+            if (item3 == item2)
+                item3++;
+            if (item3 == Taple.Length)
+                item3 -= 2;
+            if (item1 == 0)
+                item1++;
+            if (item2 == 0)
+                item2 += 2;
+            if (item3 == 0)
+                item3 += Taple.Length - 1;
+            if (start == false)
+                Console.WriteLine("the ShopKeeper: " + ShopKeeperIn[RNG.Next(0, ShopKeeperIn.Length)]);
+            itemsList[ItemsListIndex].name = Taple[item1];
+            itemsList[ItemsListIndex].Con = Con[item1];
+            itemsList[ItemsListIndex].cost = cost[item1];
+            ItemsListIndex++;
+            itemsList[ItemsListIndex].name = Taple[item2];
+            itemsList[ItemsListIndex].Con = Con[item2];
+            itemsList[ItemsListIndex].cost = cost[item2];
+            ItemsListIndex++;
+            itemsList[ItemsListIndex].name = Taple[item3];
+            itemsList[ItemsListIndex].Con = Con[item3];
+            itemsList[ItemsListIndex].cost = cost[item3];
+            En = true;
+        }
+        void S_WCIB()
+        {
+            Console.WriteLine("The ShopKeeper: " + "i have");
+            for (int i = 0; i < itemsList.Length; i++)
+            {
+                Console.WriteLine(itemsList[i].Con + " " + itemsList[i].name + " for " + itemsList[i].cost + ", ");
+            }
+        }
+        void S_Buy()
+        {
+            Console.WriteLine("the ShopKeeper: " + "what to buy?");
+            Console.WriteLine("to a buy a thing you need to say the item number eg " + "item1 to buy " + Con[item1] + " " + Taple[item1] + " for " + cost[item1]);
+            string UserBuy = Console.ReadLine();
+            #region Buy Item1
+            if (UserBuy == "item1" || UserBuy == "Item1")
+            {
+                if (Player.coins >= cost[item1])
+                {
+                    Player.inv[Player.InvIndex] = Taple[item1];
+                    Player.invCon[Player.InvIndex] = Con[item1];
+                    Player.InvIndex++;
+                    Console.WriteLine("the ShopKeeper: " + "thank you for purchaseing " + Con[item1] + " " + Taple[item1] + " for " + cost[item1]);
+                    Player.coins -= cost[item1];
+                    Taple[item1] = "";
+                    Con[item1] = 0;
+                    cost[item1] = 0;
+                }
+                else
+                    Console.WriteLine("the ShopKeeper: " + "Sorry but you do not have enough coins you need " + cost[item1] + " coins");
+            }
+            #endregion
+            #region Buy Item2
+            if (UserBuy == "item2" || UserBuy == "Item2")
+            {
+                if (Player.coins >= cost[item2])
+                {
+                    Player.inv[Player.InvIndex] = Taple[item2];
+                    Player.invCon[Player.InvIndex] = Con[item2];
+                    Player.InvIndex++;
+                    Console.WriteLine("the ShopKeeper: " + "thank you for purchaseing " + Con[item2] + " " + Taple[item2] + " for " + cost[item2]);
+                    Player.coins -= cost[item2];
+                    Taple[item2] = "";
+                    Con[item2] = 0;
+                    cost[item2] = 0;
+                }
+                else
+                    Console.WriteLine("the ShopKeeper: " + "Sorry but you do not have enough coins you need " + cost[item2] + " coins");
+            }
+            #endregion
+            #region Buy Item3
+            if (UserBuy == "item3" || UserBuy == "Item3")
+            {
+                if (Player.coins >= cost[item3])
+                {
+                    Player.inv[Player.InvIndex] = Taple[item3];
+                    Player.invCon[Player.InvIndex] = Con[item3];
+                    Player.InvIndex++;
+                    Console.WriteLine("the ShopKeeper: " + "thank you for purchaseing " + Con[item3] + " " + Taple[item3] + " for " + cost[item3]);
+                    Player.coins -= cost[item3];
+                    Taple[item3] = "";
+                    Con[item3] = 0;
+                    cost[item3] = 0;
+                }
+                else
+                    Console.WriteLine("the ShopKeeper: " + "Sorry but you do not have enough coins you need " + cost[item3] + " coins");
+            }
+            #endregion
+        }
+        bool EnC = false;
+        public void Craft(bool Start)
+        {
+            string Help = @"you can say ""what can i craft"" or ""wcic""";
+            if(EnC == false)
+            {
+                Console.WriteLine(@"what to do say ""help"" and get help");
+                EnC = true;
+            }
+            string UserCraft = Console.ReadLine();
+            if (UserCraft == "Help" || UserCraft == "help")
+                Console.WriteLine(Help);
+            if (UserCraft == "what can i craft" || UserCraft == "wcic" || UserCraft == "WCIC")
+                Console.WriteLine(craft.CraftintItems);
+            if (UserCraft == "Craft" || UserCraft == "craft")
+            {
+                Console.WriteLine("what to craft");
+                string HTC = Console.ReadLine();
+                if(HTC == "SP")
+                    craft.Craft(HTC);
+            }
+            if (UserCraft == "goout" || UserCraft == "GoOut" || UserCraft == "GO")
+            {
+                EnC = false;
+            }
+            if (EnC == true)
+                Craft(Start);
         }
     }
 }
--- crafting/CraftItems.cs
+++ crafting/CraftItems.cs
@@ -10,29 +10,106 @@ namespace game_in_console.crafting
     [Serializable]
     public class CraftingRe
     {
-        public int Slot1ItemID;
-        public int Slot2ItemID;
-        public int Slot3ItemID;
-        public int Slot4ItemID;
-        public int Slot5ItemID;
-        public int Slot6ItemID;
-        public int Slot7ItemID;
-        public int Slot8ItemID;
-        public int Slot9ItemID;
+        public string Slot1ItemName;
+        public string Slot2ItemName;
+        public string Slot3ItemName;
+        public string Slot4ItemName;
+        public string Slot5ItemName;
     }
     public class CraftItems
     {
-        CraftingRe[] CraftingRe;
+        int SlotItemNameIndex = 2;
+        public string[] Slot1ItemName;
+        public string[] Slot2ItemName;
+        public string[] Slot3ItemName;
+        public string[] Slot4ItemName;
+        public string[] Slot5ItemName;
         Mat mat;
+        public Player Player;
+        public string CraftintItems = "Stone pickaxe for 3 Stones and 2 Sticks";
         public void Start()
         {
+            Slot1ItemName = new string[SlotItemNameIndex];
+            Slot2ItemName = new string[SlotItemNameIndex];
+            Slot3ItemName = new string[SlotItemNameIndex];
+            Slot4ItemName = new string[SlotItemNameIndex];
+            Slot5ItemName = new string[SlotItemNameIndex];
             mat = new Mat();
-            CraftingRe = new CraftingRe[2];
-            CraftingRe[0].Slot1ItemID = 1;
-            CraftingRe[0].Slot2ItemID = 1;
-            CraftingRe[0].Slot3ItemID = 1;
-            CraftingRe[0].Slot5ItemID = 3;
-            CraftingRe[0].Slot8ItemID = 3;
+            Slot1ItemName[0] = "Stones";
+            Slot2ItemName[0] = "Sticks";
+        }
+        public void Craft(string Item)
+        {
+            if(Item == "SP" || Item == "StonePickaxe" || Item == "stonepickaxe")
+            {
+                for (int i = 0; i < Player.InvIndex; i++)
+                {
+                    int II = i + 1;
+                    int IIM = i - 1;
+                    if(Player.inv[II] == Slot1ItemName[0] && Player.inv[i] == Slot2ItemName[0] ||
+                        Player.inv[i] == Slot1ItemName[0] && Player.inv[II] == Slot2ItemName[0])
+                    {
+                        if(Player.invCon[i] >= 2 && Player.invCon[II] >= 3 ||
+                            Player.invCon[i] >= 3 && Player.invCon[II] >= 2)
+                        {
+                            if (Player.inv[i] == "Stones")
+                            {
+                                Player.invCon[i] -= 3;
+                            }
+                            else if (Player.inv[i] == "Sticks")
+                            {
+                                Player.invCon[i] -= 2;
+                            }
+                            Player.inv[i] = "";
+                            if (i != 0)
+                            {
+                                if (Player.inv[IIM] == "Stones")
+                                {
+                                    Player.invCon[IIM] -= 3;
+                                }
+                                else if(Player.inv[IIM] == "Sticks")
+                                {
+                                    Player.invCon[IIM] -= 2;
+                                }
+                                Player.inv[IIM] = "";
+                            }
+                            else
+                            {
+                                if (Player.inv[II] == "Stones")
+                                {
+                                    Player.invCon[II] -= 3;
+                                }
+                                else if (Player.inv[II] == "Sticks")
+                                {
+                                    Player.invCon[II] -= 2;
+                                }
+                                Player.inv[II] = "";
+                            }
+                            int III = i + 2;
+                            if (Player.invCon[i] == 0)
+                            {
+                                Player.invCon[i] = 1;
+                                Player.inv[i] = "StonePickaxe";
+                            }
+                            else if (Player.invCon[II] == 0)
+                            {
+                                Player.invCon[II] = 1;
+                                Player.inv[II] = "StonePickaxe";
+                            }
+                            else if(Player.invCon[IIM] == 0)
+                            {
+                                Player.invCon[IIM] = 1;
+                                Player.inv[IIM] = "StonePickaxe";
+                            }
+                            else if (Player.invCon[III] == 0)
+                            {
+                                Player.invCon[III] = 1;
+                                Player.inv[III] = "StonePickaxe";
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
